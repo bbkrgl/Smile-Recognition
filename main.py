06ab0b4c6a1c44bba4aed7c1456a86b4d7312089
Burak Köroğlu <burak20012009@gmail.com>
2018-06-13 23:08:36 +0300

    First

---
+++ main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+import Tkinter as tk
+from PIL import Image, ImageTk
+import trainer
+import json
+
+"""
+rs = json.load(open("results.xml"))
+trn = trainer.Trainer()
+trn.results = rs
+"""
+
+window = tk.Tk()
+window.wm_title("Smile Recognition")
+window.config(background="#FFFFFF")
+
+imageFrame = tk.Frame(window, width=600, height=500)
+imageFrame.grid(row=0, column=0, padx=10, pady=2)
+text = tk.Label(window, text="isSmiling Text")
+
+text.grid(row=1, column=0)
+
+lmain = tk.Label(imageFrame)
+lmain.grid(row=0, column=0)
+cap = cv2.VideoCapture(0)
+
+
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    filename = "/home/brkkrgl/PycharmProjects/Smile Recognition/lbpcascade_frontalface.xml"
+    classifier = cv2.CascadeClassifier(filename)
+    faces = classifier.detectMultiScale(cv2image)
+    for x, y, w, h in faces:
+        cv2.rectangle(cv2image, (x, y), (x+w, y+h), (255,0,0), 2)
+
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_frame)
+
+
+sliderFrame = tk.Frame(window, width=600, height=100)
+sliderFrame.grid(row=600, column=0, padx=10, pady=2)
+
+
+show_frame()
+window.mainloop()
---
+++ trainer.py
@@ -0,0 +1,64 @@
+from sklearn import datasets
+import Tkinter as tk
+from PIL import Image, ImageTk
+from sklearn.svm import SVC
+import json
+
+face_dataset = datasets.fetch_olivetti_faces()
+svc = SVC(kernel="linear")
+
+
+class Trainer:
+    def __init__(self):
+        self.results = {}
+        self.imgs = face_dataset.images
+
+        self.index = 0
+
+    def increment_face(self):
+        if self.index + 1 >= len(self.imgs):
+            return self.index
+        else:
+            while str(self.index) in self.results:
+                print self.index
+                self.index += 1
+            return self.index
+
+    def record_result(self, smile=True):
+        self.results[str(self.index)] = smile
+
+
+trainer = Trainer()
+
+window = tk.Tk()
+
+window.wm_title("Smile Recognition")
+window.config(background="#FFFFFF")
+
+
+def display_face(face):
+    img = Image.fromarray(face)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain = tk.Label(window)
+    lmain.configure(image=imgtk)
+    lmain.image = imgtk
+    lmain.grid(row=0, column=0)
+
+
+def update_smiling(b):
+    trainer.record_result(smile=b)
+    trainer.increment_face()
+
+smiling_button = tk.Button(window, text="Smiling", command=lambda x=True: update_smiling(x))
+smiling_button.grid(row=1, column=0)
+nonsmiling_button = tk.Button(window, text="Not Smiling", command=lambda x=False: update_smiling(x))
+nonsmiling_button.grid(row=1, column=1)
+
+display_face(trainer.imgs[trainer.index])
+
+window.mainloop()
+
+""""
+with open("results.xml", "w") as f:
+    json.dump(trainer.results, f)
+"""
